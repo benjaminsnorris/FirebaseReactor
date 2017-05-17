bbd68efaf6ee897d4796b5360df0400c96a0cd93
Ben Norris <code@bsn.design>
2017-05-17 16:39:20 -0600

    Add Firebase framework

--- FirebaseReactor/FirebaseAccess.swift
+++ FirebaseReactor/FirebaseAccess.swift
@@ -6,5 +6,14 @@
  */
 
 import Foundation
+import Firebase
 import Reactor
 import Marshal
+
+public protocol FirebaseAccess {
+    
+    /// The base ref for your Firebase app
+    var ref: DatabaseReference { get }
+    var currentApp: FirebaseApp? { get }
+    
+}
